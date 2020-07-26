7e867958fc6bb04547afa2d403b9f3fa19a3c5fd
Zainab Omar <zainab.alshaikhli01@gmail.com>
2020-07-26 05:14:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,13 +10,12 @@ class Dog
   end
   def self.print_all
  @@all.each do |dog_name|
-   
-  
+   puts dog_name.name
+ end
 end
-#def self.save (name)
- # @name=name
- # @@all << self
-#end
+def self.save (name)
+ @@all << self
+end
 def self.clear_all
   @@all.clear
 end
